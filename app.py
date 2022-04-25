be94e5a34ddee6d88407e79ceb8ab4af386c6573
Muhammad Hazieq <muhammadhazieq00@gmail.com>
2022-04-26 00:13:31 +0800

    added new db file

--- app.py
+++ app.py
@@ -4,20 +4,13 @@ import logging
 from flaskCreate import app
 from errorhandling import error
 import MySQLdb.cursors
-import re
-import secrets
-import os
 
 
 logging.basicConfig(filename='record.log', level=logging.DEBUG, format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
 
-# app.secret_key = secrets.token_hex(16)
-# database Initialize in another file
-# app.config['MYSQL_HOST'] = 'us-cdbr-east-05.cleardb.net'
-# app.config['MYSQL_USER'] = 'bb09d462c08d07'
-# app.config['MYSQL_PASSWORD'] = '2979179c'
-# app.config['MYSQL_DB'] = 'heroku_4176c403d4233e5'
-# mysql = MySQL(app)
+
+# mysql initialization
+mysql = MySQL(app)
 
 @app.route('/')
 def index():
@@ -28,27 +21,40 @@ def index():
 @app.route('/profile')
 def profile():
     #TODO:  check for session log in first
+    app.logger.info('Fetch my-profile.html template')
     return render_template('my-profile.html')
 
 @app.route('/application')
 def application():
     #TODO:  check for session log in first
+    app.logger.info('Fetch job-application.html template')
     return render_template('job-application.html')
 
 @app.route('/joblist')
 def joblist():
     #TODO:  check for session log in first
+    app.logger.info('Fetch job-list.html template')
     return render_template('job-list.html')
 
 @app.route('/jobview')
 def jobview():
     #TODO:  check for session log in first
+    app.logger.info('Fetch job-view.html template')
     return render_template('job-view.html')
 
 @app.route('/newjob')
 def newjob():
     #TODO:  check for session log in first
+    app.logger.info('Fetch new-job.html template')
     return render_template('new-job.html')
+
+
+@app.route('/submitNewJobPost', methods=["GET","POST"])
+def submitNewJobPost():
+    position = request.values.get('data')
+    print(position,"adsdadsdadadad")
+    app.logger.info('position: ',position)
+    return "1"
 
 
 if __name__ == '__main__':
--- flaskCreate.py
+++ flaskCreate.py
@@ -1,5 +1,12 @@
-from flask import Flask, render_template, request, redirect, url_for, session,abort
+from flask import Flask
+import secrets
 
 app = Flask(__name__)
 
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+app.secret_key = secrets.token_hex(16)
+app.config['MYSQL_HOST'] = '127.0.0.1'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '@Aa0174552687'
+app.config['MYSQL_DB'] = 'hiringSystem'
