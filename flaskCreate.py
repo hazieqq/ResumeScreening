9bfe5285a3032cf0ea0a1aab6f17599ea0d42c42
Muhammad Hazieq <muhammadhazieq00@gmail.com>
2022-06-02 22:52:37 +0800

    temporary disable db

--- flaskCreate.py
+++ flaskCreate.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 app.secret_key = secrets.token_hex(16)
-app.config['MYSQL_HOST'] = '127.0.0.1'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '@Aa0174552687'
-app.config['MYSQL_DB'] = 'hiringSystem'
+# app.config['MYSQL_HOST'] = '127.0.0.1'
+# app.config['MYSQL_USER'] = 'root'
+# app.config['MYSQL_PASSWORD'] = '@Aa0174552687'
+# app.config['MYSQL_DB'] = 'hiringSystem'
